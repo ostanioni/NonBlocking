41d6ec8ddc9bd3de5783697032bea472e4b07060
ostanioni <ostanioni@gmail.com>
2021-03-02 15:05:13 +0700

    0.5.1  connected WS

---
+++ client.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+
+console.clear();
+const ws = new WebSocket('ws://127.0.0.9:3009', {
+  perMessageDeflate: false
+});
+
+ws.on('open', function open() {
+  console.log('\x1b[30m\x1b[44m%s\x1b[0m', 'Web Socket Client running ... ');
+  ws.send('Hello, Server!');
+});
+
+ws.on('message', function incoming(msg) {
+  ws.send(msg);
+});
+
