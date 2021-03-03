a4bc9cf9c21c5ffd340fa0184b6a60a73a82bd09
Sergio Ostanioni <ostanioni@gmail.com>
2021-03-03 19:34:11 +0700

    perfomance.now() added

--- JavaScript/2-each-non-blocking.js
+++ JavaScript/2-each-non-blocking.js
@@ -1,9 +1,14 @@
 'use strict';
 
+// let startTime = window.performance.now();
+// let elapsedTime = performance.now() - startTime;
+
 const N = 100;
 // const last = array.length - 1;
 const last = N - 1;
 const numbers = new Array(N).fill(1);
+
+let startTime = performance.now();
 
 const each = (array, fn) => {
   const next = i => {
@@ -32,3 +37,6 @@ each(numbers, (item, i) => {
     console.log( '\x1b[36m%s\x1b[0m', `Benchmark took ${diff} ms`);
   };
 });
+
+let elapsedTime = performance.now() - startTime;
+console.log(`$Elapsed time: ${elapsedTime}`)
