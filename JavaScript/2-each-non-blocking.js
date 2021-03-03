a50459344fdeca29c2b6205f1b457b56c801af22
ostanioni <ostanioni@gmail.com>
2021-03-03 07:57:18 +0700

    v 0.5

--- JavaScript/2-each-non-blocking.js
+++ JavaScript/2-each-non-blocking.js
@@ -5,16 +5,6 @@ const N = 100;
 const last = N - 1;
 const numbers = new Array(N).fill(1);
 
-function each(array, func){
-  function next(i){
-    setTimeout(()=> {
-      func(array[i], i);
-      if (i !== last) next(++i);
-    },0)
-  }
-  next(0)
-}
-
 const each = (array, fn) => {
   const next = i => {
     setTimeout(() => {
@@ -31,13 +21,14 @@ const timer = setInterval(() => {
 }, 10);
 
 const begin = process.hrtime.bigint();
+console.time(`Benchmark`);
 each(numbers, (item, i) => {
   console.log(i);
   if (i === last) {
     clearInterval(timer);
     
+    console.timeEnd(`Benchmark`);
     const diff = (process.hrtime.bigint() - begin) / 1000000n;
     console.log( '\x1b[36m%s\x1b[0m', `Benchmark took ${diff} ms`);
-    // console.log('Time(ms):', diff.toString());
   };
 });
